2c8a3a198cbf357c61edac6a3b1d1e5437112722
dilip26thapa <dilip26thapa@gmail.com>
2022-07-22 16:06:20 +0545

    e-commerce_website

---
+++ Hello/home/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
